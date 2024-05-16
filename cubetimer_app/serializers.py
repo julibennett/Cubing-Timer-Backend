406587a69bc026577b25c37c7956254d674a8797
julibennett <julibennett4@gmail.com>
2024-05-16 14:07:57 -0700

    degugging Friendship model

--- cubetimer_app/serializers.py
+++ cubetimer_app/serializers.py
@@ -22,8 +22,13 @@ class FriendshipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Friendship
         fields = ['id', 'creator', 'friend', 'created_at']
+        read_only_fields = ('creator', 'created_at')  # 'creator' and 'created_at' should not be set by the user
 
     def validate(self, data):
-        if data['creator'] == data['friend']:
+        if data['friend'] == self.context['request'].user:
             raise serializers.ValidationError("Users cannot be friends with themselves.")
         return data
+
+    def create(self, validated_data):
+        validated_data['creator'] = self.context['request'].user
+        return Friendship.objects.create(**validated_data)
