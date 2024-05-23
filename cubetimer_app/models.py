e7b4f63403338cb7f215a4d0736ce5a7484db874
julibennett <julibennett4@gmail.com>
2024-05-23 10:19:43 -0700

    quick change

--- cubetimer_app/models.py
+++ cubetimer_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Add a ManyToMany field to the User model for starred users
+
 User.add_to_class('starred_users', models.ManyToManyField(
     'self',
     through='cubetimer_app.StarredUser',
